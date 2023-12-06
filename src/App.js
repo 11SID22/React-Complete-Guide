5be589363325b6f3bcc2ea8d5d5a530e23e3bba2
11SID22 <sid111097@gmail.com>
2023-12-07 05:02:48 +0530

    To call, used map to iterate through multiple obj.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import ExpenseItem from "./components/ExpenseItem";
 
-
 function App() {
   const expenses = [
     {
@@ -10,11 +9,12 @@ function App() {
       date: new Date(2020, 7, 14),
       location: 'PVR Oberoi (Mumbai)'
     },
-    { id: 'e2', 
-    title: 'New Bike', 
-    amount: 400000, 
-    date: new Date(2021, 2, 12),
-    location: 'BMW motors (Mumbai)'
+    {
+      id: 'e2',
+      title: 'New Bike',
+      amount: 400000,
+      date: new Date(2021, 2, 12),
+      location: 'BMW motors (Mumbai)'
     },
     {
       id: 'e3',
@@ -35,31 +35,14 @@ function App() {
   return (
     <div>
       <h2>Expense Items:</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-        location={expenses[0].location}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-        location={expenses[1].location}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-        location={expenses[2].location}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-        location={expenses[3].location}
-      ></ExpenseItem>
-      
+      {expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          location={expense.location}
+        />
+      ))}
     </div>
   );
 }
