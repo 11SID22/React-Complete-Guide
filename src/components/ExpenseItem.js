488ce20bc18332d49078ce31abe10ee853f607b7
11SID22 <sid111097@gmail.com>
2023-12-07 04:01:24 +0530

    Styling, using JS in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,21 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <div>
-        <div>Expense Items:</div>
-        <div>Volleyball: Rs 600<div>
-        </div>Football: Rs 400</div>
-        <div>Bat & Ball: Rs 1000</div>
-    </div>
+    const expenseDate = new Date(2023, 11, 25);
+    const expenseTitle = 'Christmas Celebration';
+    const expenseAmount = 10000;
+    const locationOfExpenditure = 'Mumbai';
+
+    return (
+        <div className='expense-item'>
+            <div>{expenseDate.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>Rs{expenseAmount}</div>
+                <div className='expense-item__price'>{locationOfExpenditure}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
