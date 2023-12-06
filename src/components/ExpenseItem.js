0f86747ea08636bc5deac4b5733beb938b5cb7f4
11SID22 <sid111097@gmail.com>
2023-12-06 21:15:54 +0530

    Added items in ExpenseItem by hard coding.

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,9 @@
+function ExpenseItem() {
+    return <h2>Expense Items: <br></br> 
+    Volleyball: Rs 600 <br></br>
+    Football: Rs 400 <br></br>
+    Bat & Ball: Rs 1000
+    </h2>
+}
+
+export default ExpenseItem;
