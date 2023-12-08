d5ab3026ebbae87acc96cb4f7e1a55ea7004bdfb
11SID22 <sid111097@gmail.com>
2023-12-08 20:31:56 +0530

    New components added, learned props, used & manipulated them to make components smaller & readable.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from "./components/ExpenseItem";
+// import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -6,28 +7,28 @@ function App() {
       id: 'e1',
       title: 'Movies',
       amount: 200,
-      date: new Date(2020, 7, 14),
+      date: new Date(2022, 4, 22),
       location: 'PVR Oberoi (Mumbai)'
     },
     {
       id: 'e2',
       title: 'New Bike',
       amount: 400000,
-      date: new Date(2021, 2, 12),
+      date: new Date(2024, 9, 11),
       location: 'BMW motors (Mumbai)'
     },
     {
       id: 'e3',
       title: 'Trip',
       amount: 2000,
-      date: new Date(2021, 2, 28),
+      date: new Date(2024, 2, 25),
       location: 'Ratnagiri'
     },
     {
       id: 'e4',
       title: 'Shopping',
       amount: 1500,
-      date: new Date(2021, 5, 12),
+      date: new Date(2021, 11, 15),
       location: 'Borivali (Mumbai)'
     },
   ];
@@ -35,14 +36,7 @@ function App() {
   return (
     <div>
       <h2>Expense Items:</h2>
-      {expenses.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          location={expense.location}
-        />
-      ))}
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,14 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 
 function ExpenseItem(props) {
-  
+
+
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>Rs{props.amount}</div>
-                <div className='expense-item__price'>{props.location}</div>
-            </div>
+            <ExpenseDate date={props.date} />
+            <ExpenseDetails title={props.title} amount={props.amount} location={props.location} />
         </div>
     );
 }
