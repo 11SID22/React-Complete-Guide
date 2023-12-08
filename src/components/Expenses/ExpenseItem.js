f6a026f947b5973d0aca075f9092f91d94a5af41
11SID22 <sid111097@gmail.com>
2023-12-09 04:44:45 +0530

    Added Click Event Listeners 'Change Title & Delete Expense'.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,12 +6,18 @@ import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
-
-
+    const clickHandler = () => {
+        console.log('Change the Title')
+    }
+    const expenseDelHandler = () => {
+        console.log('Expense is deleted')
+    }
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <ExpenseDetails title={props.title} amount={props.amount} location={props.location} />
+            <button onClick={clickHandler}>Change Title</button> 
+            <button onClick={expenseDelHandler}>Delete Expense</button>
         </Card>
     );
 }
