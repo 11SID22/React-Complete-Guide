512df6ea4e5ffd416bbb2fd39742e88d2a0b6e1e
11SID22 <sid111097@gmail.com>
2023-12-12 01:53:15 +0530

    Corrected the previous commit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -54,13 +54,14 @@ const ExpenseForm = () => {
         // })
     }
     const submitHandler = (event) => {
-        event.preventDefaut();
+        event.preventDefault();
 
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
             date: new Date(enteredDate)
         }
+        console.log(expenseData);
     }
 
     return (
