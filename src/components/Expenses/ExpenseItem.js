aa41ae67a050045a9bc0ed67e65428382086b98b
11SID22 <sid111097@gmail.com>
2023-12-12 14:05:57 +0530

    Added filter component, used props by lifting state up & removed those demo buttons from ExpenseItem.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react';
 
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
@@ -6,27 +6,10 @@ import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
-    const [amount, setAmount] = useState(props.amount);
-
-    const clickHandler = () => {
-        setTitle('Updated!');
-        console.log('Change the Title');
-    }
-    const updateAmountHandler = () => {
-        setAmount('100');
-    }
-    const expenseDelHandler = () => {
-        console.log('Expense is deleted')
-    }
-
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
-            <ExpenseDetails title={title} amount={amount} location={props.location} />
-            <button onClick={clickHandler}>Change Title</button>
-            <button onClick={expenseDelHandler}>Delete Expense</button>
-            <button onClick={updateAmountHandler}>Update Amount</button>
+            <ExpenseDetails title={props.title} amount={props.amount} location={props.location} />
         </Card>
     );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,37 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
+    const [filterYear, setFilterYear] = useState('2020');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilterYear(selectedYear);
+    }
     return (
-        <Card className="expenses">
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}
-                location={props.items[0].location}
-            />
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}
-                location={props.items[1].location}
-            />
-            <ExpenseItem
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}
-                location={props.items[2].location}
-            />
-            <ExpenseItem
-                title={props.items[3].title}
-                amount={props.items[3].amount}
-                date={props.items[3].date}
-                location={props.items[3].location}
-            />
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter selected={filterYear} onChangeFilter={filterChangeHandler} />
+                <ExpenseItem
+                    title={props.items[0].title}
+                    amount={props.items[0].amount}
+                    date={props.items[0].date}
+                    location={props.items[0].location}
+                />
+                <ExpenseItem
+                    title={props.items[1].title}
+                    amount={props.items[1].amount}
+                    date={props.items[1].date}
+                    location={props.items[1].location}
+                />
+                <ExpenseItem
+                    title={props.items[2].title}
+                    amount={props.items[2].amount}
+                    date={props.items[2].date}
+                    location={props.items[2].location}
+                />
+                <ExpenseItem
+                    title={props.items[3].title}
+                    amount={props.items[3].amount}
+                    date={props.items[3].date}
+                    location={props.items[3].location}
+                />
+            </Card>
+        </div>
     )
 
 }
